e168f21d35f82eab4e97c672d325f1d37ced2888
humpedd <humphreytarra@gmail.com>
2024-05-02 14:56:06 +0800

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Note;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => bcrypt('pass123')
+        ]);
+        User::factory()->create([
+            'id' => 4,
+            'name' => 'Humphjrey',
+            'email' => 'humps@example.com',
+            'password' => bcrypt('pass123')
+        ]);
+
+        Note::factory(100) -> create();
+    }
+}
